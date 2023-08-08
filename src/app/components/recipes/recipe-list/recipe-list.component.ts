152036983a63532bd4c11c4fc24ebc02e4aa8234
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-08-08 17:38:58 +0200

    StyleRecipe3

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -7,9 +7,16 @@ import { Recipe } from 'src/app/Models/recipe';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-  recipes: Recipe = new Recipe(
-    'A Test Recipe',
-    'This is simply test',
-    'https://media.homecentre.com/i/homecentre/161298546-161298546-HC140219_01-2100.jpg?fmt=auto&$quality-standard$&sm=c&$prodimg-d-sqr-pdp-2x$'
-  );
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply test',
+      'https://media.homecentre.com/i/homecentre/161298546-161298546-HC140219_01-2100.jpg?fmt=auto&$quality-standard$&sm=c&$prodimg-d-sqr-pdp-2x$'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply test',
+      'https://media.homecentre.com/i/homecentre/161298546-161298546-HC140219_01-2100.jpg?fmt=auto&$quality-standard$&sm=c&$prodimg-d-sqr-pdp-2x$'
+    ),
+  ];
 }
