a37c7674140eb9721bb1c652cc2d50d9560ceaf6
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-08-08 17:21:35 +0200

    StyleRecipe2

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Recipe } from 'src/app/Models/recipe';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-  recipes = [];
-
+  recipes: Recipe = new Recipe(
+    'A Test Recipe',
+    'This is simply test',
+    'https://media.homecentre.com/i/homecentre/161298546-161298546-HC140219_01-2100.jpg?fmt=auto&$quality-standard$&sm=c&$prodimg-d-sqr-pdp-2x$'
+  );
 }
