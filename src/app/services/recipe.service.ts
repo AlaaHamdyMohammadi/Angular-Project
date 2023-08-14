22d1785aa83b76c972f869d8725a0b2ac107b881
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-08-14 18:54:53 +0200

    ServiceWorkDropdownList

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from '../Models/recipe';
+import { Ingredient } from '../Models/ingredient';
 
 @Injectable({
   providedIn: 'root',
@@ -11,18 +12,20 @@ export class RecipeService {
     new Recipe(
       'A Test Recipe1',
       'This is simply test1',
-      'https://media.homecentre.com/i/homecentre/161298546-161298546-HC140219_01-2100.jpg?fmt=auto&$quality-standard$&sm=c&$prodimg-d-sqr-pdp-2x$'
+      'https://media.homecentre.com/i/homecentre/161298546-161298546-HC140219_01-2100.jpg?fmt=auto&$quality-standard$&sm=c&$prodimg-d-sqr-pdp-2x$',
+      [new Ingredient('Meet', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       'A Test Recipe2',
       'This is simply test2',
-      'https://media.homecentre.com/i/homecentre/161298546-161298546-HC140219_01-2100.jpg?fmt=auto&$quality-standard$&sm=c&$prodimg-d-sqr-pdp-2x$'
+      'https://media.homecentre.com/i/homecentre/161298546-161298546-HC140219_01-2100.jpg?fmt=auto&$quality-standard$&sm=c&$prodimg-d-sqr-pdp-2x$',
+      [new Ingredient('Meet', 1), new Ingredient('Bread', 2)]
     ),
   ];
 
-  getRecipes(){
+  getRecipes() {
     //To get shallow copy of array
     return this.recipes.slice();
-  };
+  }
   constructor() {}
 }
